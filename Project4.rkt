56360ac86b941bda5dd4aadf1e02a591bc667ff5
Vncentou422 <Vncentou422@gmail.com>
2016-11-06 00:29:53 -0400

    Update Project4.rkt
    works without error now

--- Project4.rkt
+++ Project4.rkt
@@ -1,7 +1,7 @@
-
 (define (perfectnum n)
-  (if (not(= n 0))
-     ((display(string-append(number->string(* (- (expt 2 n) 1) (expt 2 (- n 1)))) "\n"))      
-      (perfectnum(- n 1)))))
+  (cond ((not(= n 0))
+         (display(string-append(number->string(* (- (expt 2 n) 1) (expt 2 (- n 1)))) "\n"))      
+         (perfectnum(- n 1)))))
+
 
 (perfectnum 100)
