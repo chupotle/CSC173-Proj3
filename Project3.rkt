0ce2ab33d0276edd601ad9f3258e379102aa7407
Vncentou422 <Vncentou422@gmail.com>
2016-11-06 17:06:13 -0500

    Add files via upload

---
+++ Project3.rkt
@@ -0,0 +1,18 @@
+(define (sumfact n i)
+  (cond
+    ((= i 1) 1)
+    ((= (remainder n i) 0)
+     (+ i (sumfact n (- i 1))))
+    (else (sumfact n (- i 1)))))
+
+(sumfact 8128 8127)
+
+
+(define (perfectnum n)
+  (cond ((not(= n 0))
+         (display(string-append(number->string(* (- (expt 2 n) 1) (expt 2 (- n 1)))) "\n"))      
+         (perfectnum(- n 1)))))
+
+
+(perfectnum 100)
+
