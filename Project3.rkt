d0ab911a6b3f911230094a52ef230a0530dccda7
chupotle <tcchu97@gmail.com>
2016-11-06 18:39:16 -0500

    done

--- Project3.rkt
+++ Project3.rkt
@@ -5,14 +5,23 @@
      (+ i (sumfact n (- i 1))))
     (else (sumfact n (- i 1)))))
 
-(sumfact 8128 (- 8129 1))
+(define (perfectnum num counter)
+  (cond
+    [(not(= counter 0))
+     (cond
+       [(= num (sumfact num (- num 1))) (display num) (display "\n")(perfectnum (+ num 1)(- counter 1))]
+       [(not(= num (sumfact num (- num 1)))) (perfectnum (+ num 1) counter)])]))
 
+(define (isPerf low high)
+  (cond
+    [(not(> low high))
+     (cond
+       [(= low (sumfact low (- low 1))) (display low)(display " perfect \n")]
+       [(< low (sumfact low (- low 1))) (display low)(display " deficient \n")]
+       [(> low (sumfact low (- low 1))) (display low)(display " abundant\n")]
+       [else #f])
+    (isPerf (+ low 1) high)]))
 
-(define (perfectnum n)
-  (cond ((not(= n 0))
-         (display(string-append(number->string(* (- (expt 2 n) 1) (expt 2 (- n 1)))) "\n"))      
-         (perfectnum(- n 1)))))
-
-
-(perfectnum 100)
-
+(perfectnum 2 3)
+(display "\n")
+(isPerf 2 1000)
