e9589663a453fb8f8cf8b7cd4567230dfc0f9757
Vincent Ou <vncentou422@gmail.com>
2016-11-06 19:44:46 -0500

    added comments to files

--- Project3.rkt
+++ Project3.rkt
@@ -1,24 +1,30 @@
+;;CSC 173 Project 3
+;;Collaborators: Timothy Chu, Vincent Ou, Griffin van de Venne
+
+;;Recursively finds the sum of the factors of a number
+;;n = the number, i = number - 1
 (define (sumfact n i)
   (cond
     ((= i 1) 1)
     ((= (remainder n i) 0)
      (+ i (sumfact n (- i 1))))
     (else (sumfact n (- i 1)))))
-
+;;Finds n perfect numbers with n = counter
 (define (perfectnum num counter)
   (cond
-    [(not(= counter 0))
+    [(not(= counter 0));; if we have not reached the desired amount of perfect numbers
      (cond
-       [(= num (sumfact num (- num 1))) (display num) (display "\n")(perfectnum (+ num 1)(- counter 1))]
-       [(not(= num (sumfact num (- num 1)))) (perfectnum (+ num 1) counter)])]))
+       [(= num (sumfact num (- num 1))) (display num) (display "\n")(perfectnum (+ num 1)(- counter 1))] ;;check if the sum of the factors is equal to the number, if so print and move on to the next number and counter
+       [(not(= num (sumfact num (- num 1)))) (perfectnum (+ num 1) counter)])]));; if not move onto the next number
 
+;;Lists numbers between low and high and list whether they are abundant perfect or deficient
 (define (isPerf low high)
   (cond
-    [(not(> low high))
+    [(not(> low high)) ;;if we reached the end of the list
      (cond
-       [(= low (sumfact low (- low 1))) (display low)(display " perfect \n")]
-       [(< low (sumfact low (- low 1))) (display low)(display " deficient \n")]
-       [(> low (sumfact low (- low 1))) (display low)(display " abundant\n")]
+       [(= low (sumfact low (- low 1))) (display low)(display " perfect \n")];;sum of factors = number
+       [(< low (sumfact low (- low 1))) (display low)(display " deficient \n")];;sum of factors < number
+       [(> low (sumfact low (- low 1))) (display low)(display " abundant\n")];;sum of factors > number
        [else #f])
     (isPerf (+ low 1) high)]))
 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CSC173-Proj3
+Groupmates Timothy Chu, Vincent Ou, Griffin van de Venne
+
+Project3.rkt is the Racket file that runs part 1 and part 2 of the project. Click Run in DrRacket in order to run the test cases for the project.
+
+There is a sumfact(n i) function that calculates the sum of the factors of the number n, with i being one unit less than n. The variable i functions as a counter of sorts. 
+
+perfectnum(num counter) counts down from the number of perfect numbers we want to print out and num is the starting number, in this case 2. This function looks at all the numbers starting from 2 and checks if it is perfect, and if it is it prints out that number and increases the counter by one.
+
+isPerf(low high) has a similar function as perfectnum as in it checks all the numbers between the low and high and checks if they are abundant, perfect, or deficient usign the sumfact(n i) function. After checking it prints out the number and the corresponding label.
